8ee6810837e2c9097a3bdc9319659e5081dd71c6
Admir Zatega <admir.zatega1009@gmail.com>
2024-07-14 02:33:05 +0200

    Initial commit

---
+++ rust_rest_api/src/main.rs
@@ -0,0 +1,34 @@
+#[macro_use] extern crate rocket;
+
+extern crate tlsn_verifier;
+use tlsn_verifier::*;
+
+use serde_json::Value;
+use std::{str};
+
+mod verify;
+mod request_opt; 
+
+#[get("/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/")]
+async fn test() -> String {
+    let pem = str::from_utf8(include_bytes!("../notary.pem")).unwrap();
+    let proof = str::from_utf8(include_bytes!(
+        "../proof.json"
+    ))
+    .unwrap();
+
+    let result = verify::verify(proof, pem).await.expect("result");
+
+    format!("I am the tester man {}", result)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/hello", routes![hello]);
+    rocket::build().mount("/test", routes![test])
+}
